f1bf6c73e7d5f1cb5ae3c0a64d912db3121a9066
723156735 <723156735@qq.com>
2019-04-03 10:08:15 +0800

    修复了enum导出报错的问题

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,9 @@
-enum loopTypes {
-  loop,
-  order,
-  shuffle
+export enum loopTypes {
+    loop,
+    order,
+    shuffle
 }
-enum playerSizeType {
-  normal,
-  mini
+export enum playerSizeType {
+    normal,
+    mini
 }
-export { loopTypes, playerSizeType };
